e2993d1b18de56145f62da9059f6c2df6bc82590
Angoliuk <43971036+Angoliuk@users.noreply.github.com>
2019-11-26 22:22:04 +0200

    Add files via upload

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,30 +1,63 @@
+let onCalc = false;
+
+function OnOffFunc() {
+
+  if (onCalc == true) {
+
+    onCalc = false
+
+  } else {
+
+    onCalc = true
+  }
+
+}
+
 function Action(sign) {
+
   return document.getElementById('input').value = document.getElementById('input').value + sign;
+
 }
 
 function Deleting() {
+
   document.getElementById('input').value = "";
+
 }
 
 function Calculating() {
+
   let exercise = document.getElementById('input').value;
-  if (exercise == "" || exercise == "+" || exercise == "." || exercise == "-" || exercise == "*" || exercise == "/" || exercise == "%" || exercise == " " || exercise == "0.0") {
-    alert('You dont enter anything or something went wrong')
+
+  if (onCalc == false) {
+
+    alert("calculator is off,press 'on/off button'")
+
   } else {
 
-    document.getElementById('input').value = eval(exercise);
+    if (exercise == "" || exercise == "+" || exercise == "." || exercise == "-" || exercise == "*" || exercise == "/" || exercise == "%" || exercise == " " || exercise == "0.0") {
+      alert('You dont enter anything or something went wrong')
 
-    if (document.getElementById('input-last-activities1').value == "") {
-      document.getElementById('input-last-activities1').value = eval(exercise);
-    } else if (document.getElementById('input-last-activities2').value == "") {
-      document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
-      document.getElementById('input-last-activities1').value = eval(exercise);
-      let something = document.getElementById('input-last-activities2').value;
     } else {
-      document.getElementById('input-last-activities3').value = document.getElementById('input-last-activities2').value;
-      document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
-      document.getElementById('input-last-activities1').value = eval(exercise);
-    };
+
+      document.getElementById('input').value = eval(exercise);
+
+      if (document.getElementById('input-last-activities1').value == "") {
+        document.getElementById('input').value = eval(exercise);
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+
+      } else if (document.getElementById('input-last-activities2').value == "") {
+        document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+        let something = document.getElementById('input-last-activities2').value;
+
+      } else {
+        document.getElementById('input-last-activities3').value = document.getElementById('input-last-activities2').value;
+        document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+      };
+
+    }
 
   }
 }

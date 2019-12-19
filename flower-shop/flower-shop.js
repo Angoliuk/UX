2d7ba1d29b238886ed9cb5a80d3d831a66aab50a
Angoliuk <43971036+Angoliuk@users.noreply.github.com>
2019-12-19 16:25:12 +0200

    Add files via upload

--- flower-shop/flower-shop.js
+++ flower-shop/flower-shop.js
@@ -13,3 +13,102 @@
         };
     }
     setInterval(carousel, 3000);
+
+    function buyFlower(name, flower, price) {
+
+
+        let imageOfProduct = document.createElement("img");
+        imageOfProduct.src = flower.src;
+
+
+        let nameOfProduct = document.createElement("p");
+        let nameOfProductText = document.createTextNode(name);
+        nameOfProduct.appendChild(nameOfProductText);
+
+
+        let priceForOne = document.createElement("p");
+        let priceForOneText = document.createTextNode("price for one :" + price + "$");
+        priceForOne.appendChild(priceForOneText);
+
+
+        let numberOfProduct = document.createElement("input");
+        numberOfProduct.disabled = true;
+        numberOfProduct.value = 1;
+
+
+        let addProduct = document.createElement("button");
+        let addProductText = document.createTextNode("+");
+        addProduct.appendChild(addProductText);
+        addProduct.onclick = function add() {
+            numberOfProduct.value++;
+        };
+
+
+        let subtractProduct = document.createElement("button");
+        let subtractProductText = document.createTextNode("-");
+        subtractProduct.appendChild(subtractProductText);
+        subtractProduct.onclick = function add() {
+            if (numberOfProduct.value > 0) {
+                numberOfProduct.value--;
+            } else {
+                numberOfProduct.value = 0;
+            }
+        };
+
+
+        let numberOfProductBlock = document.createElement("div");
+        numberOfProductBlock.appendChild(subtractProduct);
+        numberOfProductBlock.appendChild(numberOfProduct);
+        numberOfProductBlock.appendChild(addProduct);
+
+
+        let totalPriceOfProduct = document.createElement("div");
+        let totalPriceOfProductText = document.createElement("p")
+        let TotalPriceText = document.createTextNode("Total price : ");
+        totalPriceOfProductText.appendChild(TotalPriceText);
+        totalPriceOfProduct.appendChild(totalPriceOfProductText);
+
+
+        let busket = document.createElement("div");
+        busket.appendChild(nameOfProduct);
+        busket.appendChild(priceForOne);
+        busket.appendChild(numberOfProductBlock);
+        busket.appendChild(totalPriceOfProduct);
+
+
+        let allBusket = document.createElement("div");
+        allBusket.appendChild(imageOfProduct);
+        allBusket.appendChild(busket);
+
+
+        allBusket.className += "row product-in-busket";
+        imageOfProduct.classList.toggle("col-lg-6");
+        busket.classList.toggle("col-lg-6");
+        busket.classList.toggle("row");
+        nameOfProduct.classList.toggle("col-lg-12");
+        priceForOne.classList.toggle("col-lg-12");
+        numberOfProductBlock.classList.toggle("col-lg-12");
+        addProduct.classList.toggle("col-lg-2");
+        numberOfProduct.classList.toggle("col-lg-8");
+        subtractProduct.classList.toggle("col-lg-2");
+
+
+        document.getElementById("busket").appendChild(allBusket);
+
+
+        let startBusket = document.getElementById("busketHidden");
+        let startTotalPrice = document.getElementById("totalPrice");
+        let startBusketText = document.getElementById("emptyBusket");
+        let startTotalPriceText = document.createTextNode("Total Price for all : ")
+        if (startBusket.contains(startBusketText)) {
+            startBusketText.remove();
+        } else {
+            return;
+        }
+        if (!startTotalPrice.contains(startTotalPriceText)) {
+            document.getElementById("totalPrice").appendChild(startTotalPriceText);
+        } else {
+            return;
+        }
+
+    }

441bf6391c59d6af8d7112e16364595c3a127da1
Stas_Angoliuk <gamongara@gmail.com>
2020-03-03 22:12:42 +0200

    update

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let onCalc = false;
 
 function OnOffFunc() {
@@ -60,4 +61,68 @@ function Calculating() {
     }
 
   }
+=======
+let onCalc = false;
+
+function OnOffFunc() {
+
+  if (onCalc == true) {
+
+    onCalc = false;
+
+  } else {
+
+    onCalc = true;
+  }
+
+}
+
+function Action(sign) {
+
+  return document.getElementById('input').value = document.getElementById('input').value + sign;
+
+}
+
+function Deleting() {
+
+  document.getElementById('input').value = "";
+
+}
+
+function Calculating() {
+
+  let exercise = document.getElementById('input').value;
+
+  if (onCalc == false) {
+
+    alert("calculator is off,press 'on/off' button")
+
+  } else {
+
+    if (exercise == "" || exercise == "+" || exercise == "." || exercise == "-" || exercise == "*" || exercise == "/" || exercise == "%" || exercise == " " || exercise == "0.0") {
+      alert('You dont enter anything or something went wrong')
+
+    } else {
+
+      document.getElementById('input').value = eval(exercise);
+
+      if (document.getElementById('input-last-activities1').value == "") {
+        document.getElementById('input').value = eval(exercise);
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+
+      } else if (document.getElementById('input-last-activities2').value == "") {
+        document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+        let something = document.getElementById('input-last-activities2').value;
+
+      } else {
+        document.getElementById('input-last-activities3').value = document.getElementById('input-last-activities2').value;
+        document.getElementById('input-last-activities2').value = document.getElementById('input-last-activities1').value;
+        document.getElementById('input-last-activities1').value = exercise + " = " + eval(exercise);
+      };
+
+    }
+
+  }
+>>>>>>> smth
 }
--- drag`n`drop projects/drag`n`drop/drag`n`drop.js
+++ drag`n`drop projects/drag`n`drop/drag`n`drop.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let fruits = document.querySelectorAll('.draggable-fruit');
 let baskets = document.querySelectorAll('.basket');
 let current;
@@ -17,4 +18,25 @@ baskets.forEach(function(baskets) {
   baskets.addEventListener('drop', function() {
     baskets.appendChild(current);
   });
+=======
+let fruits = document.querySelectorAll('.draggable-fruit');
+let baskets = document.querySelectorAll('.basket');
+let current;
+
+fruits.forEach(function(fruits) {
+  fruits.addEventListener('dragstart', function() {
+    current = this;
+  });
+});
+
+baskets.forEach(function(baskets) {
+
+  baskets.addEventListener('dragover', function(event) {
+    event.preventDefault();
+  });
+
+  baskets.addEventListener('drop', function() {
+    baskets.appendChild(current);
+  });
+>>>>>>> smth
 })

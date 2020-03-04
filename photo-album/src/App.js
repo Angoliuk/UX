3f6bb1f69348a48f1ee16e65c9c18c10f7097918
Jean Marc Mbouma <jeanmarcm@instem.com>
2020-03-04 20:31:02 +0200

    Getting images from remote endpoint rather than hard-coded photos collection

--- photo-album/src/App.js
+++ photo-album/src/App.js
@@ -1,59 +1,42 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-const photos = [{
-    id : 1,
-    src: "https://picsum.photos/seed/picsum/250/350"
-  },
-  {
-    id : 2,
-    src: "https://picsum.photos/250/350/?blur=3"
-  },
-  {
-    id : 3,
-    src: "https://picsum.photos/seed/picsum/250/350"
-  },
-  {
-    id : 4,
-    src: "https://picsum.photos/250/350?grayscale"
-  },
-  {
-    id : 5,
-    src: "https://picsum.photos/250/350/?blur=1.5"
-  },
-  {
-    id : 6,
-    src: "https://picsum.photos/id/870/250/350?grayscale&blur=2"
-  },
-  {id: 7,
-    src: "https://picsum.photos/seed/picsum/250/350"
-  }, {
-    id: 8,
-    src: "https://picsum.photos/250/350/?blur"
-  }, {
-    id: 9,
-    src: "https://picsum.photos/seed/picsum/250/350"
-  }, {
-    id: 10,
-    src: "https://picsum.photos/250/350?grayscale"
-  }, {
-    id: 11,
-    src: "https://picsum.photos/250/350/?blur=2"
-  }, {
-    id: 12,
-    src: "https://picsum.photos/id/870/250/350?grayscale&blur=2"
-  },
-];
 
-function App() {
-  return (
+const uri = "https://picsum.photos/v2/list";
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      images: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(uri).then(result => result.json())
+      .then(jsonImages => this.setState({
+        images: jsonImages
+      }))
+  }
+
+  render() {
+     return (
     <div>
       <Header /> 
-      {photos.map(item => {
-       return <img key={item.id} className="part-of-photo-list" src={item.src} id={item.id} />
+      {this.state.images.map(item => {
+       return <img key={item.id}
+        className="part-of-photo-list"
+         src={item.download_url}
+          id={item.id} 
+          width="300"
+          height="200"
+          />
       })}
     </div>
-  );
+    )
+  }
+ 
 }
 
 export default App;
